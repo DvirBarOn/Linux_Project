4e5e06e528c996539004476bcdab4ae01634f8d5
noamcohen622 <noamcohen622@gmail.com>
2026-04-23 17:59:21 +0300

    change mains on both and adding GUI for visual

--- main.c
+++ main.c
@@ -1,10 +1,6 @@
-// #include <stdio.h>
-//
-// int main(void) {
-//     printf("Hello, dvir Bar On!\n");
-//     printf("my name is ITAY2\n");
-//     printf("my name is Noam");
-//     printf("elie");
-//     return 0;
-//
-// }
+void runGraphVisualizer(void);
+
+int main(void) {
+    runGraphVisualizer();
+    return 0;
+}
